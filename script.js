2de985bec0f368e655773044794da27319d37252
teresabat <contatotommymateus@gmail.com>
2025-05-22 00:38:11 -0300

    terminado com js pronto e css ajustado

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // pega o campo do display da calculadora
+const display = document.getElementById('display');
+
+//add o valor clicando no botao ao display
+function appendValue(value){
+	display.value += value;
+}
+
+//limpa o display (botao c)
+function clearDisplay(){
+	display.value = '';
+}
+
+//faz o calculo com base na expressao do display
+function calculate(){
+	try{
+		display.value = eval(display.value);
+	} catch (error) {
+		display.value = 'Erro';
+	}
+}
+
